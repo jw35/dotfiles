5aeeace777852bcb8ddf15cc907262908490fbbd
Jon Warbrick <jw35@cam.ac.uk>
2018-11-30 16:24:32 +0000

    Add some ESLint rules relating to spacing

--- eslintrc.js
+++ eslintrc.js
@@ -42,5 +42,21 @@ module.exports = {
         "no-trailing-spaces": "error",
         "eol-last": "error",
         "no-console": "warn",
+	"comma-spacing": "error",
+        "no-unused-vars": "warn",
+        "array-bracket-spacing": "error",
+        "func-call-spacing": "error",
+        "key-spacing": [
+            "error",
+            { "mode": "minimum" }
+        ],
+        "semi-spacing": "error",
+        "switch-colon-spacing": "error",
+        "arrow-spacing": "error",
+        "template-curly-spacing": "error",
+        "no-multiple-empty-lines": [
+            "error",
+            { "max": 2 }
+        ]
     }
 };
